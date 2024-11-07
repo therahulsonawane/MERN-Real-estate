78b1741854fe04fe7cd4bb9523966a303a31a12c
therahulsonawane <rahulsonwane7066@gmail.com>
2024-11-07 07:55:23 +0530

    adding functionality to singup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,30 +1,46 @@
 import { useState } from "react";
 import Signin from "../pages/Signin";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Signup() {
   const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
   const handlechange = (e) => {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
   };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // send form data to server
-    const res = await fetch("/server/auth/signup/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
-    const data = await res.json();
-    console.log(data);
+    try {
+      setLoading(true);
+      // send form data to server
+      const res = await fetch("/server/auth/signup/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/signin");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
   };
-
-  console.log(formData);
 
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -51,8 +67,11 @@ export default function Signup() {
           id="password"
           onChange={handlechange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Sign Up
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -61,6 +80,7 @@ export default function Signup() {
           <span className="text-blue-700">Sign In</span>
         </Link>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,11 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    "/server": {
-      target: "http://localhost:3000",
-      secure: false,
+    proxy: {
+      "/server": {
+        target: "http://localhost:3000",
+        secure: false,
+      },
     },
   },
   plugins: [react()],
